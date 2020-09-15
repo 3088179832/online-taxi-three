684697834776b9b24e1061f1da11688a242d7174
3088179832 <3088179832@qq.com>
2020-09-15 11:37:43 +0800

    online-taxi-threee init

---
+++ service-verification-code/src/main/java/com/mashibing/serviceverificationcode/controller/VerigyCodeController.java
@@ -0,0 +1,15 @@
+package com.mashibing.serviceverificationcode.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/verify-code")
+public class VerigyCodeController {
+
+    public String generate(){
+
+
+        return "";
+    }
+}
